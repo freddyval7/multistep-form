3f157757f5b90f70f689a63d46c87865d16a9439
Freddy Tomada <freddytomada7@gmail.com>
2024-02-19 09:52:37 -0400

    cut border in step 5

--- app/step5.tsx
+++ app/step5.tsx
@@ -4,7 +4,7 @@ import check from "../public/icon-thank-you.svg";
 
 export default function Step5() {
   return (
-    <div className="text-center flex flex-col items-center md:w-1/2 mx-auto my-auto border-2 rounded-md shadow-lg p-8 space-y-8">
+    <div className="text-center flex flex-col items-center md:w-1/2 mx-auto my-auto p-8 space-y-8">
       <Image className="w-16 h-16" width={24} height={24} src={check} alt="check" />
       <h1 className="text-2xl font-bold text-marine-blue">Thank you!</h1>
       <p className="text-cool-gray">
