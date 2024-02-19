c129669d80c6773b128310c5a262fa8f4dc35858
Freddy Tomada <freddytomada7@gmail.com>
2024-02-19 09:04:34 -0400

    add step 5

--- app/step5.tsx
+++ app/step5.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
+import check from "../public/icon-thank-you.svg";
+
 export default function Step5() {
   return (
-    <div>Step5</div>
-  )
+    <div className="text-center flex flex-col items-center md:w-1/2 mx-auto my-auto border-2 rounded-md shadow-lg p-8 space-y-8">
+      <Image className="w-16 h-16" width={24} height={24} src={check} alt="check" />
+      <h1 className="text-2xl font-bold text-marine-blue">Thank you!</h1>
+      <p className="text-cool-gray">
+        Thanks for confirming your subscription! We hope you have fun using our
+        platform. If you ever need support, please feel free to email us at
+        support@loremgaming.com.
+      </p>
+    </div>
+  );
 }
